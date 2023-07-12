620c83a323ad621e81f99ff0ea217d6a94c83350
DurstDog87 <durstdog87@durstdog>
2023-07-12 14:07:28 -0400

    pre rm push

--- src/__tests__/tileserver.ts
+++ src/__tests__/tileserver.ts
@@ -1,1 +1,14 @@
 import { Tileserver } from "../index"
+import { Pool } from "pg"
+import "dotenv/config"
+
+const pool = new Pool({
+    user: process.env.TEST_USER,
+    password: process.env.TEST_PWD,
+    database: process.env.TEST_DB,
+    host: process.env.HOST,
+    port: 5432
+})
+
+
+const tileserver = new Tileserver(pool)
