95264a4dd4e44d6ce2603d719301c4c5c6e8edcc
Micah Meaindertsma <durstdog87@gmail.com>
2023-11-13 11:32:37 -0500

    parameterize z,x,y

--- src/index.ts
+++ src/index.ts
@@ -1,77 +1,75 @@
-import { Pool, QueryResult } from "pg"
-import { IQueryOptions } from "./types"
+import { Pool, QueryResult } from 'pg';
+import { IQueryOptions } from './types';
 
-export const DEFAULT_SRID = 4269
-export const DEFAULT_EXTENT = 4096
-export const DEFAULT_BUFFER = 256
-export const DEFAULT_CLIP_GEOM = true
+export const DEFAULT_SRID = 4269;
+export const DEFAULT_EXTENT = 4096;
+export const DEFAULT_BUFFER = 256;
+export const DEFAULT_CLIP_GEOM = true;
 
 export class Tileserver {
-    pool: Pool
-    srid: number
-    extent: number
-    buffer: number
-    clip_geom: boolean
+  pool: Pool;
+  srid: number;
+  extent: number;
+  buffer: number;
+  clip_geom: boolean;
 
-    declare queryString: string
+  declare queryString: string;
 
-    constructor(pool: Pool) {
-        this.pool = pool 
-        this.srid = DEFAULT_SRID
-        this.extent = DEFAULT_EXTENT
-        this.buffer = DEFAULT_BUFFER
-        this.clip_geom = DEFAULT_CLIP_GEOM
+  constructor(pool: Pool) {
+    this.pool = pool;
+    this.srid = DEFAULT_SRID;
+    this.extent = DEFAULT_EXTENT;
+    this.buffer = DEFAULT_BUFFER;
+    this.clip_geom = DEFAULT_CLIP_GEOM;
+  }
+
+  setPool(pool: Pool) {
+    this.pool = pool;
+  }
+
+  setQuery(query: string) {
+    this.queryString = query;
+  }
+
+  setSrid(srid: number) {
+    this.srid = srid;
+  }
+
+  async query(z: number, x: number, y: number, options: IQueryOptions = {}): Promise<ArrayBuffer> {
+    if (z === undefined || x === undefined || y === undefined) {
+      throw EvalError('tile coordinates not defined');
     }
 
-    setPool(pool: Pool) {
-        this.pool = pool
+    if (!this.queryString && !options.queryString) {
+      throw EvalError('no query string set');
     }
 
-    setQuery(query: string) {
-        this.queryString = query
-    }
-
-    setSrid(srid: number) {
-        this.srid = srid
-    }
-
-    async query(z:number, x:number, y:number, options: IQueryOptions = {}):
-    Promise<ArrayBuffer> {
-
-        if (z===undefined || x===undefined || y===undefined) {
-            throw EvalError("tile coordinates not defined")
-        }
-
-        if(!this.queryString && !options.queryString) {
-            throw EvalError("no query string set")
-        }
-
-        const query = `
+    const query = `
         WITH mvtgeom AS (
             SELECT ST_AsMVTGeom(
                 ST_Transform(geom, 3857), 
-                ST_TileEnvelope(${z},${x},${y}), 
-                ${options.extent??this.extent},
-                ${options.buffer??this.buffer},
-                ${options.clip_geom??this.clip_geom}
+                ST_TileEnvelope($1,$2,$3), 
+                ${options.extent ?? this.extent},
+                ${options.buffer ?? this.buffer},
+                ${options.clip_geom ?? this.clip_geom}
                 ) AS mvtgeom, dat.*
-            FROM (${(options.queryString??this.queryString).replace(/;$/g, "")}) AS dat
+            FROM (${(options.queryString ?? this.queryString).replace(/;$/g, '')}) AS dat
             WHERE ST_Intersects(
                 geom, 
-                ST_Transform(ST_TileEnvelope(${z},${x},${y}), ${options.srid??this.srid}))
+                ST_Transform(ST_TileEnvelope($1,$2,$3), ${options.srid ?? this.srid}))
         )
-        SELECT ST_AsMVT(mvtgeom.*, '${options.layerName??"default"}') AS mvt FROM mvtgeom;
-        `
+        SELECT ST_AsMVT(mvtgeom.*, '${options.layerName ?? 'default'}') AS mvt FROM mvtgeom;
+        `;
 
-        const conn = await this.pool.connect()
+    const conn = await this.pool.connect();
 
-        try{
-            const result: QueryResult<{mvt:ArrayBuffer}> = await conn.query(query, options.params??[])
-            return result.rows[0].mvt
-        } catch(e) {
-            throw e
-        } finally {
-            conn.release()
-        }
+    try {
+      const result: QueryResult<{ mvt: ArrayBuffer }> = await conn.query(query, [z, x, y]);
+      return result.rows[0].mvt;
+    } catch (e) {
+      throw e;
+    } finally {
+      conn.release();
     }
+  }
 }
